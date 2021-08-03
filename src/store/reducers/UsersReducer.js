0b40dec618c13ae4395499eddb09609fba3256df
Nadja2310 <maksakova.nadja@gmail.com>
2021-08-03 16:24:26 +0200

    signUpIn

--- src/store/reducers/UsersReducer.js
+++ src/store/reducers/UsersReducer.js
@@ -1,6 +1,23 @@
+import {
+  ADD_NEW_USER,
+  CHANGE_AUTH,
+} from "../typesList";
+
 export const UsersReducer = (state = {}, action) => {
   switch (action.type) {
-    default:
-      return state;
+
+      case ADD_NEW_USER:
+          return {
+              ...state,
+              list: [...state.list, action.payload],
+          };
+      case CHANGE_AUTH:
+          return {
+              ...state,
+              isAuth: action.payload,
+          };
+
+      default:
+          return state;
   }
-};
+}
