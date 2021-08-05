1ea2813f0d0884a1222eb3855651a458bb129577
Nadja2310 <maksakova.nadja@gmail.com>
2021-08-06 01:38:17 +0200

    add reaction like

--- src/store/actions/act_posts.js
+++ src/store/actions/act_posts.js
@@ -1,25 +1,17 @@
-import { FETCH_POSTS, ADD_POST, FETCH_POST_BY_ID } from "../typesList";
+import { FETCH_POSTS, ADD_POST, FETCH_POST_BY_ID, EDIT_POST } from "../typesList";
 import { URL } from "../utilites";
 
 export const getPosts = () => {
 
-    return async (dispatch) => {
-        try {
-            const response = await fetch(`${URL}/posts`, {
-                method: "GET",
-                mode: "cors",
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            });
-            const data = await response.json();
-
-            await dispatch(fetchPosts({ list: data }));
-        } catch (err) {
-            console.log(err.message);
-
+  return async (dispatch) => {
+    try {
+      const response = await fetch(`${URL}/posts`, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json"
         }
-      );
+      });
       const data = await response.json();
 
       await dispatch(fetchPosts({ list: data }));
@@ -50,45 +42,74 @@ export const addNewPost = (newPost) => {
 };
 
 const fetchPosts = (obj) => {
-    return {
-        type: FETCH_POSTS,
-        payload: obj,
-    };
+  return {
+    type: FETCH_POSTS,
+    payload: obj,
+  };
 };
 
 export const getPostById = (postId) => {
-    return async dispatch => {
-        try {
-            const response = await fetch(`${URL}/posts/${postId}`, {
-                method: "GET",
-                mode: "cors",
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            });
-            if (response.status !== 200) {
-                return
-            }
-            const data = await response.json();
-            console.log("id");
-            console.log(data);
-            await dispatch(fetchPostById(data));
-        } catch (err) {
-            console.log(err.message);
+  return async dispatch => {
+    try {
+      const response = await fetch(`${URL}/posts/${postId}`, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json"
         }
+      });
+      if (response.status !== 200) {
+        return
+      }
+      const data = await response.json();
+      console.log("id");
+      console.log(data);
+      await dispatch(fetchPostById(data));
+    } catch (err) {
+      console.log(err.message);
     }
+  }
 }
 const fetchPostById = (post) => {
-    return {
-        type: FETCH_POST_BY_ID,
-        payload: post,
-    }
+  return {
+    type: FETCH_POST_BY_ID,
+    payload: post,
+  }
 }
 
 const addNewPostToState = (post) => {
   return {
     type: ADD_POST,
-    payload: post, 
+    payload: post,
   };
 };
+export const addLikeToPost = (id) => {
+  return async (dispatch, getState) => {
+    const post = getState().posts.list.find((post) => post.id === id);
+    //const post = getState().posts.postById;
 
+    try {
+      const response = await fetch(`${URL}/posts/${post.id}`, {
+        method: "PUT",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": localStorage.token
+        },
+        body: JSON.stringify({ like: post.like + 1 })
+
+      });
+      if (response.status !== 200) {
+        return
+      }
+
+      const data = await response.json();
+      await dispatch(changePostInState(data))
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+}
+const changePostInState = (post) => {
+  return { type: EDIT_POST, payload: post }
+}
